26e36a6d130dea3d0de2b5d73e8f6137ee3a39d3
nampham90 <namandroid.it@gmail.com>
2021-05-13 20:44:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/BDSHUE-APP'));
 
-app.use(express.static(path.join(__dirname, 'dist','BDSHUE-APP'));
-
-
 app.get('/*', (req, res) =>
     res.sendFile(path.join(__dirname,'dist','BDSHUE-APP','index.html'));
 );
